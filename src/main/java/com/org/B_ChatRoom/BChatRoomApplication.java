fb3aaf61e2313374a4db381488aa32a25bc75770
Shib <Shib@DESKTOP-MI3DU86>
2024-03-09 22:45:51 +0530

    First Commit

---
+++ src/main/java/com/org/B_ChatRoom/BChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.org.B_ChatRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BChatRoomApplication.class, args);
+	}
+
+}
